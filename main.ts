296fb11b7e1c6205a7bb308748f02662073341ab
Drin436 <115304694+Drin436@users.noreply.github.com>
2022-10-07 21:05:47 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 1; index++) {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
+basic.forever(function () {
+	
+})
